d22f3b5f5df6e0cca8bf71ef37e6f31232db5084
kreker3113 <balandinaleksey8@gmail.com>
2024-09-28 11:44:51 +0300

    Add files via upload

---
+++ KASD/KASD/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace KASD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string file1 = "C:\\Users\\Uzer\\Documents\\file.txt";
+            try
+            {
+                StreamReader streamReader = new StreamReader(file1);
+                uint dimension = uint.Parse(streamReader.ReadLine());
+                double[][] tensorMatrix = new double[dimension][];
+                
+                
+                for (int i = 0; i < dimension; i++)
+                {  
+                    tensorMatrix[i] = streamReader.ReadLine().Split(' ').Select(x => double.Parse(x)).ToArray();
+                }
+                if (Symmetry(tensorMatrix))
+                {
+                    double[][] vector = new double[1][];
+                    vector[0] = streamReader.ReadLine().Split(' ').Select(x => double.Parse(x)).ToArray();
+                    VectorLench(dimension, vector, tensorMatrix);
+                  
+                }
+                else
+                {
+                    Console.WriteLine("матрица не является симетричной");
+                    streamReader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex}");
+            }
+            Thread.Sleep(10000);
+        }
+        public static bool Symmetry(double[][] tensorMatrix)
+        {
+            for(int i = 0;i < tensorMatrix.Length;i++)
+            {
+                for (int j = 0; j < tensorMatrix[i].Length; j++)
+                {
+                    if (tensorMatrix[i][j] != tensorMatrix[j][i]) return false;
+                }  
+            }
+            return true;
+        }
+        public static void VectorLench(uint dimension, double[][] vector, double[][] tensorMatrix)
+        {
+            double[] mas = new double[dimension];
+            double res = 0;
+            for (int i = 0; i < tensorMatrix.Length; i++)
+            {
+                mas[i] = vector[0][i] * tensorMatrix[i][i];
+            }
+            double[][] Tvector = new double[dimension][]; 
+            for (int i = 0; i < dimension; i++)
+            {
+                Tvector[i] = new double[1];
+            }
+            for (int i = 0; i < dimension; i++)
+            {
+                Tvector[i][0] = vector[0][i];
+            }
+            for(int i = 0;i < dimension; i++)
+            {
+                res += mas[i] * Tvector[i][0];
+            }
+            res = Math.Sqrt(res);
+            Console.WriteLine($"Длина вектора {res}");
+            
+        }
+    }
+}
---
+++ KASD/KASD/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("KASD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KASD")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("fb8f7230-f2ff-4603-8bb9-ebd78b0e0a31")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
