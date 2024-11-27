dbf8879bfbbd370cb28c79671f7f996143884e2a
kreker3113 <balandinaleksey8@gmail.com>
2024-11-27 11:23:49 +0300

    Add files via upload

---
+++ KASD12/KASD12/Class1.cs
@@ -0,0 +1,234 @@
+﻿using KASD10;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using KASD10;
+using KASD12;
+
+namespace KASD11
+{
+    class MyPriorityQueue<T> where T : IComparable<T>
+    {
+        private T[] queue;
+        private int size;
+        private Comparer<T> comparator;
+        public MyPriorityQueue()
+        {
+            queue = new T[11];
+            size = 0;
+            comparator = Comparer<T>.Default;
+        }
+        public MyPriorityQueue(T[] A)
+        {
+            size = A.Length;
+            queue = new T[size];
+            for (int i = 0; i < size; i++)
+                queue[i] = A[i];
+            comparator = Comparer<T>.Default;
+            Rebuild();
+        }
+        public MyPriorityQueue(int initialCapacity)
+        {
+            queue = new T[initialCapacity];
+            size = 0;
+            comparator = Comparer<T>.Default;
+        }
+        public MyPriorityQueue(int initialCapacity, Comparer<T> comparator)
+        {
+            queue = new T[initialCapacity];
+            size = 0;
+            this.comparator = comparator;
+        }
+        public MyPriorityQueue(MyPriorityQueue<T> Q)
+        {
+            queue = Q.ToArray();
+            size = Q.Size();
+            comparator = Q.ComparatorGet();
+        }
+        public void Print()
+        {
+            for (int i = 0; i < size; i++)
+                Console.Write(queue[i] + " ");
+            Console.Write('\n');
+        }
+        public Comparer<T> ComparatorGet()
+        {
+            return comparator;
+        }
+        public void ComparatorSet(Comparer<T> comparator)
+        {
+            this.comparator = comparator;
+        }
+        private void Rebuild()
+        {
+            MyHeap<T> myHeap = new MyHeap<T>(queue, size);
+            myHeap.ComparatorSet(comparator);
+            for (int i = 0; i < size; i++)
+                queue[i] = myHeap.Get(i);
+        }
+        public void Add(T element)
+        {
+            if (size < queue.Length)
+            {
+                queue[size] = element;
+                size++;
+                Rebuild();
+                return;
+            }
+            int newSize;
+            if (queue.Length < 64)
+                newSize = queue.Length + 2;
+            else
+                newSize = (int)(queue.Length * 1.5);
+            T[] newQueue = new T[newSize];
+            for (int i = 0; i < size; i++)
+                newQueue[i] = queue[i];
+            newQueue[size] = element;
+            queue = newQueue;
+            size++;
+            Rebuild();
+        }
+        public void AddAll(T[] A)
+        {
+            for (int i = 0; i < A.Length; i++)
+                Add(A[i]);
+        }
+        public void Clear()
+        {
+            size = 0;
+        }
+        public bool Contains(object obj)
+        {
+            for (int i = 0; i < size; i++)
+                if (comparator.Compare((T)obj, queue[i]) == 0)
+                    return true;
+            return false;
+        }
+        public bool ContainsAll(T[] A)
+        {
+            bool flag;
+            for (int i = 0; i < A.Length; i++)
+            {
+                flag = false;
+                for (int j = 0; j < size; j++)
+                    if (comparator.Compare(A[i], queue[j]) == 0)
+                        flag = true;
+                if (!flag)
+                    return false;
+            }
+            return true;
+        }
+        public bool IsEmpty()
+        {
+            return size == 0;
+        }
+        public void Remove(object obj)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (comparator.Compare((T)obj, queue[i]) == 0)
+                {
+                    for (int j = i; j < size - 1; j++)
+                        queue[j] = queue[j + 1];
+                    size--;
+                    i--;
+                }
+            }
+            Rebuild();
+        }
+        public void RemoveAll(T[] A)
+        {
+            for (int i = 0; i < A.Length; i++)
+                Remove(A[i]);
+        }
+        public void RetainAll(T[] A)
+        {
+            bool flag;
+            for (int i = 0; i < size; i++)
+            {
+                flag = false;
+                for (int j = 0; j < A.Length; j++)
+                    if (comparator.Compare(A[i], queue[j]) == 0)
+                        flag = true;
+                if (!flag)
+                    Remove(A[i]);
+            }
+            Rebuild();
+        }
+        public int Size()
+        {
+            return size;
+        }
+        public T[] ToArray()
+        {
+            T[] A = new T[size];
+            for (int i = 0; i < size; i++)
+                A[i] = queue[i];
+            return A;
+        }
+        public void ToArray(ref T[] A)
+        {
+            if (A == null)
+            {
+                A = ToArray();
+                return;
+            }
+            if (A.Length == size)
+            {
+                for (int i = 0; i < size; i++)
+                    A[i] = queue[i];
+                return;
+            }
+            A = new T[size];
+            for (int i = 0; i < size; i++)
+                A[i] = queue[i];
+        }
+        public T Element()
+        {
+            if (size == 0)
+                throw new ArgumentOutOfRangeException("Index");
+            return queue[0];
+        }
+        private int Amount(T obj)
+        {
+            int amount = 0;
+            for (int i = 0; i < size; i++)
+                if (comparator.Compare(obj, queue[i]) == 0)
+                    amount++;
+            return amount;
+        }
+        public bool Offer(T obj)
+        {
+            int oldAmount = Amount(obj);
+            Add(obj);
+            int newAmount = Amount(obj);
+            if (oldAmount != newAmount)
+                return true;
+            return false;
+        }
+        public T Peek()
+        {
+            if (size == 0)
+                return default;
+            return queue[0];
+        }
+        public T Poll()
+        {
+            if (size == 0)
+                return default;
+            T element = queue[0];
+            for (int i = 0; i < size - 1; i++)
+                queue[i] = queue[i + 1];
+            size--;
+            Rebuild();
+            return element;
+        }
+    }
+}
---
+++ KASD12/KASD12/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using KASD11;
+
+namespace KASD12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string FILENAME = "log.txt";
+            try
+            {
+                StreamWriter streamWriter = new StreamWriter(FILENAME);
+                int n = int.Parse(Console.ReadLine());
+                MyPriorityQueue<Request> queue = new MyPriorityQueue<Request>();
+                queue.ComparatorSet(Comparer<Request>.Create(Comparison));
+                Random random = new Random();
+                Stopwatch stopwatch = new Stopwatch();
+                Tuple[] times =
+                    new Tuple[n * 10 + 1];
+                Request request;
+                Request lastRequest;
+                Tuple time;
+                int m, k = 1;
+                stopwatch.Start();
+                for (int i = 1; i <= n; i++)
+                {
+                    m = random.Next(1, 11);
+                    for (int j = 1; j <= m; j++)
+                    {
+                        request = new Request(random.Next(1, 6), k, i);
+                        queue.Add(request);
+                        time = new Tuple
+                            (stopwatch.Elapsed.TotalMilliseconds,
+                            request.priority,
+                            request.number,
+                            request.step);
+                        times[k] = time;
+                        streamWriter.Write($"ADD " +
+                            $"{request.number} " +
+                            $"{request.priority} " +
+                            $"{request.step}\n");
+                        k++;
+                    }
+                    lastRequest = queue.Poll();
+                    double waitingTime = stopwatch.Elapsed.TotalMilliseconds -
+                        times[lastRequest.number].time;
+                    time = new Tuple
+                        (waitingTime,
+                        lastRequest.priority,
+                        lastRequest.number,
+                        lastRequest.step);
+                    times[lastRequest.number] = time;
+                    streamWriter.Write($"REMOVE " +
+                        $"{lastRequest.number} " +
+                        $"{lastRequest.priority} " +
+                        $"{lastRequest.step}\n");
+                }
+                while (!queue.IsEmpty())
+                {
+                    lastRequest = queue.Poll();
+                    double waitingTime = stopwatch.Elapsed.TotalMilliseconds -
+                        times[lastRequest.number].time;
+                    time = new Tuple
+                        (waitingTime,
+                        lastRequest.priority,
+                        lastRequest.number,
+                        lastRequest.step);
+                    times[lastRequest.number] = time;
+                    streamWriter.Write($"REMOVE " +
+                        $"{lastRequest.number} " +
+                        $"{lastRequest.priority} " +
+                        $"{lastRequest.step}\n");
+                }
+                stopwatch.Stop();
+                int maxIndex = 1;
+                for (int i = 2; i < times.Length; i++)
+                    if (times[i].time > times[maxIndex].time)
+                        maxIndex = i;
+                Console.Write("Максимальное время ожидания " + "заявки: " + times[maxIndex].time + "\n");
+                Console.Write("Номер заявки: " + times[maxIndex].number + "\n");
+                Console.Write("Приоритет заявки: " + times[maxIndex].priority + "\n");
+                Console.Write("Номер шага: " + times[maxIndex].step + "\n");
+                streamWriter.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        static int Comparison(Request request1, Request request2)
+        {
+            return request1.priority - request2.priority;
+        }
+    }
+    struct Request : IComparable<Request>
+    {
+        public int priority;
+        public int number;
+        public int step;
+        public Request(int priority, int number, int step)
+        {
+            this.priority = priority;
+            this.number = number;
+            this.step = step;
+        }
+        public int CompareTo(Request request)
+        {
+            return priority - request.priority;
+        }
+    }
+    struct Tuple
+    {
+        public double time;
+        public int priority;
+        public int number;
+        public int step;
+        public Tuple(double time, int priority, int number, int step)
+        {
+            this.time = time;
+            this.priority = priority;
+            this.number = number;
+            this.step = step;
+        }
+    }
+}
